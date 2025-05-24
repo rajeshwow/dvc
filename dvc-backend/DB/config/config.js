d43da346182ef2a128a1aace5bde3955130e5499
rajesh Prajapat <rprajapat@thb.co.in>
2025-05-24 15:36:33 +0400

    mongodb uri

--- dvc-backend/DB/config/config.js
+++ dvc-backend/DB/config/config.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // mongoose.connect("mongodb://localhost:27017/DVC");
+
+const isProd = process.env.NODE_ENV === "production";
+const dbURI = isProd
+  ? process.env.MONGODB_URI_PROD
+  : process.env.MONGODB_URI_LOCAL;
+
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
