d2388628bee1cdd87ce31b999c60ca348bca49d7
rajesh Prajapat <rprajapat@thb.co.in>
2025-05-24 22:09:49 +0400

    register fix

--- dvc-backend/DB/routes/user.routes.js
+++ dvc-backend/DB/routes/user.routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.get("/", userController.getAllUsers);
-router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 router.post("/login", userController.loginUser);
