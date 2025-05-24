7a822ae386e5c8fa48729ed87125e24283948a0f
rajesh Prajapat <rprajapat@thb.co.in>
2025-05-24 16:24:23 +0400

    REACT_APP_API_BASE_URL

--- dvc-backend/DB/services/api.js
+++ dvc-backend/DB/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Create a base instance of axios with default configuration
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 const api = axios.create({
   baseURL: API_URL,
