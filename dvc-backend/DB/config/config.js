077643ba4a1eab05c64595af00542ed7f685509f
rajesh Prajapat <rprajapat@thb.co.in>
2025-05-24 15:12:41 +0400

    git ignore added

--- dvc-backend/DB/config/config.js
+++ dvc-backend/DB/config/config.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/DVC");
+require("dotenv").config();
+
+// mongoose.connect("mongodb://localhost:27017/DVC");
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
