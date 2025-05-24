a33918f3f610d969e30256616abb70197a9a189a
rajesh Prajapat <rprajapat@thb.co.in>
2025-05-24 16:36:15 +0400

    api in console

--- dvc-frontend/src/services/api.js
+++ dvc-frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 // Create a base instance of axios with default configuration
 const API_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+console.log("api url ", API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
